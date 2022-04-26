937cc520f1035952fbe2eea490de3af91c33423a
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-04-26 22:00:53 +0530

    Add files via upload

--- misaligned.c
+++ misaligned.c
@@ -1,42 +1,53 @@
-#include "misaligned.h"
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
 
-#define Max_CountInEachColorTable 5
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+
+struct Colorpair
+{
+	int Number;
+	char MajorColor[8];
+	char MinorColor[8];
+};
+
+Colorpair colorpair[25];
+
+void printOnConsole(int ColorPairArrayIndex, char *majorColor, char *minorColor)
+{
+printf("%d | %s | %s\n", ColorPairArrayIndex, majorColor, minorColor);
+}
 
 int printColorMap() {
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+    
+    int i = 0, j = 0,ColorPairArrayIndex = 1;
+    for(i = 0; i < 5; i++) 
+	{
+        for(j = 0; j < 5; j++)
+		{
+			
+			colorpair[ColorPairArrayIndex].Number = ColorPairArrayIndex;
+			strcpy(colorpair[ColorPairArrayIndex].MajorColor , majorColor[i]);
+			strcpy(colorpair[ColorPairArrayIndex].MinorColor , minorColor[j]);
+			printOnConsole(ColorPairArrayIndex,colorpair[ColorPairArrayIndex].MajorColor,colorpair[ColorPairArrayIndex].MinorColor);	
+		        ColorPairArrayIndex++;
+				          
         }
     }
     return i * j;
 }
 
-int validateColorPairFromNumber(int pairNumber, char *majorColorName, char *minorColorName)
-{
-    //colourPair paircolor;
-    int returnValue = 0;
-    int zeroBasedPairNumber = pairNumber - 1;
-    char MajorColor[8] = 
-        majorColor[(zeroBasedPairNumber / Max_CountInEachColorTable)];
-    char MinorColor[8] =
-        minorColor[(zeroBasedPairNumber % Max_CountInEachColorTable)];
-    //return colorPair;
-    if (strcmp(MajorColor, majorColorName) && strcmp(MajorColor, minorColorName))
-    {
-        returnValue = 1;
-    }
-    return returnValue;
-}
-
 int main() {
     int result = printColorMap();
-    //int validPair = validateColorNumberPair();
-    printf("%d", result);
     assert(result == 25);
-    int a=validateColorPairFromNumber(23, "Violet", "Green");
-    assert(a==0);
-    printf("All is well (maybe!)\n");
+	assert(colorpair[23].Number == 23);	
+	assert(strcmp(colorpair[23].MajorColor, "Violet") == 0);
+	assert(strcmp(colorpair[23].MinorColor, "Green") == 0);
+	assert(colorpair[15].Number == 15);	
+	assert(strcmp(colorpair[15].MajorColor, "Black") == 0);
+	assert(strcmp(colorpair[15].MinorColor, "Slate") == 0);
+    printf("All is well (Yes,it is! :> )\n");
     return 0;
 }
